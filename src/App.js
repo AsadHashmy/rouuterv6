0023ace2434826d5bc4601258e3f2a6467a21d61
Asadhashmy <asad@cloudornate.com>
2022-12-01 19:43:23 +0500

    router version 6

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { Link, NavLink, Route, Routes, useRoutes } from "react-router-dom";
+import "./App.css";
+import BookLayout from "./BookLayout";
+import Book from "./pages/Book";
+import Booklist from "./pages/Booklist";
+import Home from "./pages/Home";
+import NewBook from "./pages/NewBook ";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  // let element = useRoutes([
+  //   {
+  //     path: "/",
+  //     element: <Home />,
+  //   },
+  //   {
+  //     path: "*",
+  //     element: <NotFound />,
+  //   },
+  // ]);
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              style={({ isActive }) => {
+                return isActive ? { color: "red" } : {};
+              }}
+              to={"/"}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/books"}>Books</NavLink>
+          </li>
+        </ul>
+      </nav>
+      {/* {element} when we use useRoutes hook we use to pass object like that. */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookLayout />}>
+          <Route index element={<Booklist />} />
+          <Route path=":id" element={<Book />} />
+          <Route path="new" element={<NewBook />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
+{
+  /* <Route path="/books" element={<Booklist />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/books/new" element={<NewBook />} /> */
+}
---
+++ src/pages/Booklist.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Booklist = () => {
+  return (
+    <>
+      <h1>Book List</h1>
+    </>
+  );
+};
+
+export default Booklist;
